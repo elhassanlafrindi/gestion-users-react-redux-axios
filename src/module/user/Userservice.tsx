cd9998098f77a499c6cda0e0ef1d774943ba2210
elhassanlafrindi <elhassan.lafrindi2020@gmail.com>
2024-11-17 16:40:07 +0100

    add setUser for inset

--- src/module/user/Userservice.tsx
+++ src/module/user/Userservice.tsx
@@ -4,4 +4,7 @@ import { Iuser } from "./user.type";
 const getUserList = async () => {
   return await axiosExport.get<Iuser[]>(ApiConfig.user);
 };
+export const setuser = async (form: Iuser) => {
+  return await axiosExport.post<Iuser>(ApiConfig.user, form);
+};
 export default getUserList;
